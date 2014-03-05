7237924ca5f6e85256d4cd47d85ece3709df20de
rome2high <mnsaigonese@gmail.com>
2014-03-05 15:20:56 -0800

    submitted this version for grading

--- prog1/prog2.c
+++ prog1/prog2.c
@@ -1,15 +1,16 @@
 /*
- * Filename: prog1.c
- * This program reads integers from keyboard than compaire
- * if the amptitude of the second half matching the first half
+ * Filename: prog2.c
+ * This program reads char[] and int[] from keyboard then sort
+ * the int[] the order based on user input
  * 
  * Author Romeo Mai
- * Assignement: Spring 2014 ICS-365 #1
+ * Assignement: Spring 2014 ICS-365 #2
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include<conio.h>
 
 /*
  * reads a list of integers from the keyboard into an array.
@@ -22,9 +23,9 @@ void read_int(int list[], int max_size, int *arrLength)
 	int i = 0;
 	int num;
 	
-	for( i = 0; i < max_size; i++ )
+	for(i = 0; i < max_size; i++)
     {
-		scanf( "%d", &num );
+		scanf("%d", &num);
 		if(num <= 0)
 		{
 			*arrLength = i;
@@ -36,61 +37,9 @@ void read_int(int list[], int max_size, int *arrLength)
 }
 
 /*
- * prints a list of integers.
- * parameter: list - the array
- * parameter: size - size of the array
- * return: 1 if true, else 0
+ * reads user sort order selection and a list of integers from the keyboard into an array.
+ * return: an integer; 1 if ascending, 0 if decending
  */
-int print_integers(int list[], int size)
-{
-    int i;
-	int j = size/2;	//start index of second sequence
-
-    printf ( "You entered: " );
-
-    for ( i = 0; i < j-1; i++ )
-    {
-		if ( list[i] <= list[i+1])
-		{
-			printf( "%d - %d vs ", list[i], list[i+1]);
-			if( list[i+j] <= list[i+j+1])
-			{
-				printf( "%d - %d \n", list[i+j], list[i+j+1]);
-			}
-			else
-			{
-				printf( "Sequences are not match");
-				return 0;
-			}
-		}
-		else if( list[i] > list[i+1] )
-		{
-			printf( "%d - %d vs ", list[i], list[i+1]);
-			if ( list[i+j] > list[i+j+1] )
-			{
-				//printf( "%d",list[4]);
-				printf( "%d - %d \n", list[i+j], list[i+j+1]);
-			}
-			else
-			{
-				printf( "Sequences are not match");
-				return 0;
-			}
-		}
-		else 
-		{
-			printf( "Sequences are not match");
-			return 0;
-		}
-
-        //printf( "%d ", list[i] );
-		return 0;
-    }
-
-    printf ( "Hot Stuff \n" );
-	return 1;
-}
-
 int GetSortOrder()
 {
 	char strInput[100];
@@ -101,8 +50,8 @@ int GetSortOrder()
 	while(1)
 	{
 		printf("Enter sort order (up | down) : ");
-		//scanf( "%s", strInput );
-		gets(strInput);		/* better method read compare whole string */
+		//scanf("%s", strInput);
+		gets(strInput);		/* better method read in whole string */
 
 		for(i = 0; strInput[i]; i++)
 		{
@@ -124,6 +73,10 @@ int GetSortOrder()
 	}
 }
 
+/*
+ * swap the the value of two pointers
+ * parameter: *p , *q
+ */
 void mySwap(int *p, int *q)
 {
 	int x;
@@ -132,13 +85,19 @@ void mySwap(int *p, int *q)
 	*q = x;
 }
 
+/*
+ * sorting an array list
+ * parameter: list[] - array
+ * parameter: arrLength - integer array length
+ * parameter: isAscending - boolean
+ */
 void bubblesort(int list[], int arrLength, int isAscending)
 {
 	int i, j;
 	int *p, *q;
 
 	for (i = arrLength; i > 0; i--)
-	  {
+	{
 		for (j = 1 ; j < i; j++)
 		{
 			if(isAscending)
@@ -160,7 +119,7 @@ void bubblesort(int list[], int arrLength, int isAscending)
 				}
 			}
 		}
-	 }
+	}
 }
 
 /*
@@ -169,20 +128,19 @@ void bubblesort(int list[], int arrLength, int isAscending)
  */
 int main()
 {
-	int list[100];// = {10, 2, 1, 3, 40, 20};
+	int list[100];
 	int max_size = 100;
 	int arrLength;
-	int i;
 	int str_maxSize;
 	char ch = 'y';
-
+	int i;
 	int isAscending;
 	
 	while(1)
 	{
 		printf("\n");
 		isAscending = GetSortOrder();
-		printf("Enter a sequence of numbers: ");
+		printf("Enter a sequence of numbers (end string with a 0): ");
 		read_int(list, max_size, &arrLength);
 
 		bubblesort(list, arrLength, isAscending);
@@ -196,9 +154,8 @@ int main()
 		ch = getch();
 		if(ch != 'y')
 		{
-			return 0;
+			return 1;
 		}
 	}
-
-	return 0;
+	return 1;
 }
