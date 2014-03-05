afd335920a953c8496b0d610d6252959552bb2f5
rome2high <mnsaigonese@gmail.com>
2014-03-04 23:28:16 -0800

    completed all function need function headers before submit

--- prog1/prog2.c
+++ prog1/prog2.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 /*
  * reads a list of integers from the keyboard into an array.
@@ -16,7 +17,7 @@
  * parameter: max_size - maximum size of the array
  * parameter: *arrElements - number of elements in the list array
  */
-void read_int(int list[], int max_size, int *arrElements)
+void read_int(int list[], int max_size, int *arrLength)
 {
 	int i = 0;
 	int num;
@@ -26,10 +27,9 @@ void read_int(int list[], int max_size, int *arrElements)
 		scanf( "%d", &num );
 		if(num <= 0)
 		{
-			*arrElements = i;
+			*arrLength = i;
 			return;
 		}
-		// else continued
 		list[i] = num;
 	}
 	return;
@@ -91,105 +91,76 @@ int print_integers(int list[], int size)
 	return 1;
 }
 
-int check_Sequences(int list[], int totalNumEnter)
+int GetSortOrder()
 {
+	char strInput[100];
+	char up[] = "up";
+	char down[] = "down";
 	int i;
-	int j;
-	int retVal;
-
-	retVal = 1;
-
-	for ( i = 0; i < totalNumEnter/2; i++ )
-    {
-		//second sequence start index
-		j = i + totalNumEnter/2;
-
-		if ( list[i] <= list[i+1])
-		{
-			retVal++;
-			if( list[j] <= list[j+1])
-			{
-				retVal--;
-			}
-		}
-		else if( list[i] > list[i+1] )
-		{
-			retVal++;
-			if ( list[j] > list[j+1] )
-			{
-				retVal--;
-			}
-		}
-    }
-
-	return retVal;
-}
-
-int check_Sequences_2(int list[], int totalNumEnter)
-{
-	int i;
-	int j;
-	int retVal;
-
-	retVal = 1;
-
-	for ( i = 0; i < totalNumEnter/2; i++ )
-    {
-		//second sequence start index
-		j = i + totalNumEnter/2;
-
-		if ( list[i] <= list[i+1])
-		{
-			retVal++;
-		}
-		if( list[j] <= list[j+1])
-		{
-			retVal--;
-		}
-		if(retVal != 1)
-		{
-			return retVal;
-		}
-    }
-
-	return retVal;
-}
-
-void read_str(char sortDir[], int str_maxSize)
-{
-	char s[2];// = "Upupup";
-	char s1[] = "##";
-
-	int i = 0;
-	int sLen;
-
-	scanf( "%s", s );
-	printf( "s = %s \n", s );
-
-	sLen = strlen(s);
-
-	for(i = 0; i< sLen;i++)
-	{
-		s1[i] = tolower(s[i]);
-	}
-
-	//*s1 = tolower(*s1);
-	printf( "s1 = %s \n", s1 );
-
-	strcpy(sortDir, s);
-
-	printf( "sortDir = %s\n", sortDir);
-
-
-	//char str[] = "Hello";
-	//char *str2;
-	//printf("str : %s \n", str);
-	//str2 = &str[0]; //now, str2 can be used anywhere str is expected
-	//printf("str2 : %s \n", str2);
-	//str2 = str; //most commonly used
-	//printf("str2 : %s \n", str2);
 	
-	return;
+	while(1)
+	{
+		printf("Enter sort order (up | down) : ");
+		//scanf( "%s", strInput );
+		gets(strInput);		/* better method read compare whole string */
+
+		for(i = 0; strInput[i]; i++)
+		{
+			strInput[i] = tolower(strInput[i]);
+		}
+
+		if(strcmp(strInput,up) == 0)
+		{
+			return 1;
+		}
+		else if (strcmp(strInput, down) == 0)
+		{
+			return 0;
+		}
+		else
+		{
+			printf("ERROR: You entered %s \n", strInput);
+		}
+	}
+}
+
+void mySwap(int *p, int *q)
+{
+	int x;
+	x = *p;
+	*p = *q;
+	*q = x;
+}
+
+void bubblesort(int list[], int arrLength, int isAscending)
+{
+	int i, j;
+	int *p, *q;
+
+	for (i = arrLength; i > 0; i--)
+	  {
+		for (j = 1 ; j < i; j++)
+		{
+			if(isAscending)
+			{
+				if (list[j-1] > list[j])
+				{
+					p = &list[j-1];
+					q = &list[j];
+					mySwap(p, q);
+				}
+			}
+			else
+			{
+				if (list[j-1] < list[j])
+				{
+					p = &list[j-1];
+					q = &list[j];
+					mySwap(p, q);
+				}
+			}
+		}
+	 }
 }
 
 /*
@@ -198,61 +169,36 @@ void read_str(char sortDir[], int str_maxSize)
  */
 int main()
 {
-    int list[100];
+	int list[100];// = {10, 2, 1, 3, 40, 20};
 	int max_size = 100;
-	int totalNumEnter;
-	
-	char sortDir[2];
+	int arrLength;
+	int i;
 	int str_maxSize;
+	char ch = 'y';
 
-
-	char str1[]="UPUPUP";
-	char str2[]="upupup";
-
-	int i = 0;
-
-	//*str1 = tolower(*str1);
-
-	for(i = 0; i<6; i++)
-	{
-		str1[i] = tolower(str1[i]);
-	}
-
-	if(strcmp(str1,str2) == 0)
-	{
-		printf("str1 and str2 are identical. \n");
-	}
-
+	int isAscending;
 	
-	
-	str_maxSize = 2;
-	totalNumEnter = 0;
-
-	read_str(sortDir, str_maxSize);
-
-	printf("input: %s", sortDir);
-
-
-	//call a by-reference functiong using operator & to change value of arrElements
-	//while (totalNumEnter < 4) {
-	//	printf( "Enter a list of 4 or more integers separete by spaces and end with 0:\n" );
-	//	read_int( list, max_size, &totalNumEnter);
-	//}
-
-	printf( "Enter a list of 4 or more integers separete by spaces and end with 0:\n" );
-	read_int( list, max_size, &totalNumEnter);
-
-	if(check_Sequences_2(list,totalNumEnter) == 1)
+	while(1)
 	{
-		printf( "[MATCH] Two sequences are order equivalent\n" );
-	}
-	else
-	{
-		printf( "[UNMATCH] Two sequences are NOT order equivalent\n" );
-	}
+		printf("\n");
+		isAscending = GetSortOrder();
+		printf("Enter a sequence of numbers: ");
+		read_int(list, max_size, &arrLength);
+
+		bubblesort(list, arrLength, isAscending);
+
+		printf("Result: \n");
+		for(i = 0; i < arrLength; i++)
+			printf("%d\n", list[i]);
 	
-	printf( "Enter a number and hit ENTER to exit the program\n" );
-	scanf( "%d", &totalNumEnter );
-    
+
+		printf("\nPress 'y' to restart the program or press anything else to exit ... ");
+		ch = getch();
+		if(ch != 'y')
+		{
+			return 0;
+		}
+	}
+
 	return 0;
 }
